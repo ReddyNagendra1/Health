172f2ef3c0712e74069b2bb5e52889e060d0c216
ReddyNagendra1 <reddynagendra2352@gmail.com>
2024-11-01 01:09:09 -0400

    I

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "antd/dist/reset.css";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login';
+import Login from './pages/login';
 import Register from './pages/register';
 import Home from './pages/Home';
 import { Toaster } from 'react-hot-toast';
